65b9dbcc51d12b7669fd9c05e1a53509e2a8ea70
joan.rosellr <joan.rosellr@gmail.com>
2019-03-22 13:53:13 +0100

    add state file

---
+++ basicSudoku/Game.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "syncFile.h"
+class Game
+{
+public:
+	Game();
+	~Game();
+private:
+	/**	Summary
+	*	This class should: 
+	*	ask the user for input
+	*	
+	*	Based on the input it should:
+	*	Delegate operations on the controller, userInterface,
+	*	and sudoku.
+	*	Load fresh from a file, optionally load its latest state.
+	*/
+};
+
---
+++ basicSudoku/syncFile.h
@@ -0,0 +1,11 @@
+#pragma once
+
+enum state
+{
+	START = 0,
+	MAIN_MENU,
+	GAME_IN_PROGRESS,
+	HELP_MODE,
+	GAME_ENDED,
+	END
+};
